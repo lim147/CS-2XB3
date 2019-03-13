99f9165e9ae8662612b25e5b7cd4fa01530243b7
Robert Vardy <vardyrj@mcmaster.ca>
2019-03-13 15:04:08 -0400

    modified:   StepSafe/ReadFiles/ReadFiles/src/Event.java 	new file:   StepSafe/ReadFiles/ReadFiles/src/ReadFile.java

--- StepSafe/ReadFiles/ReadFiles/src/Event.java
+++ StepSafe/ReadFiles/ReadFiles/src/Event.java
@@ -42,6 +42,42 @@ public class Event implements Comparable<Event>{
 	public String getId() {
 		return this.Id;
 		}
-	
 
+	public int getYear() {
+		return this.Year;
+		}
+
+	public String getMonth() {
+		return this.Month;
+		}
+	
+	public int getDay() {
+		return this.Day;
+	}
+
+	public int getHour() {
+		return this.Hour;
+	}
+	
+	public String getMCI() {
+		return this.MCI;
+	}
+	
+	public double getLat() {
+		return this.Lat;
+	}
+	
+	public double getLong() {
+		return this.Long;
+	}
+	
+	public static void main(String[] args) {
+		Event rob = new Event("1", 1, "Jan", 1, 1, "Hello", 1.2, 1.3);
+		Event zach = new Event("2", 2, "Jan", 2, 2, "Hello", 2.2, 2.3);
+		System.out.println(rob.getDay());
+		System.out.println(rob.distTo);
+		
+	}
 }
+
+
