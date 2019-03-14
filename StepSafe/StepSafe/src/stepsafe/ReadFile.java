fc5a69f0726d2537be6395d9bfbfcccc36f4b4d2
May <May@MaydeMacBook-Pro.local>
2019-03-13 22:42:10 -0400

    Combine with abstrct object

--- StepSafe/StepSafe/src/stepsafe/ReadFile.java
+++ StepSafe/StepSafe/src/stepsafe/ReadFile.java
@@ -5,13 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadFile {
-	private static int SIZE = 150000;
-	
-	public static Event[] read() throws IOException {
-		Event[] dbase = new Event[SIZE];
-		File f = new File("Data\\MCI_2014_to_2017.csv");
+	public static void read_from_db(CrmALst dbase) throws NumberFormatException, IOException {
+		
+		//read from database
+		File f = new File("Data/MCI_2014_to_2017.csv");
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
 		String line;
@@ -22,10 +23,26 @@ public class ReadFile {
 				continue;
 			}
 			String[] temp = line.split(",");
-			dbase[count - 1] = new Event(temp[0], Integer.parseInt(temp[2]), temp[3], Integer.parseInt(temp[4]), Integer.parseInt(temp[7]), temp[8], new Point2D(Double.parseDouble(temp[12]), Double.parseDouble(temp[13])));
+			Event e = new Event(temp[0], Integer.parseInt(temp[2]), temp[3], Integer.parseInt(temp[4]), Integer.parseInt(temp[7]), temp[8], new Point2D(Double.parseDouble(temp[12]), Double.parseDouble(temp[13])));
+			dbase.addEvent(e);
 			count++;
 		}
-		return dbase;
+		fr.close();
 	}
+	
+	
+	
+	//test:
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		//create a CrmALst instance:
+		CrmALst dbase = new CrmALst();
+		//apply function to load data to dbase:
+		read_from_db(dbase);
+		//dbase.next();
+		System.out.println(dbase.current().toString());
+		
+		
+	}
+		
 
 }
