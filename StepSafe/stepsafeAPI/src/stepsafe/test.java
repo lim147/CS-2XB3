39881291fc61392c42907af5d5020636e3e49591
May <May@MaydeMacBook-Pro.local>
2019-04-04 17:34:37 -0400

    Update

--- StepSafe/stepsafeAPI/src/stepsafe/test.java
+++ StepSafe/stepsafeAPI/src/stepsafe/test.java
@@ -4,15 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import types.*;
 
 public class test {	
-	public static ArrayList<ArrayList<Double>> path(String s, String t) throws IOException, InterruptedException {
+	public static ArrayList<Path> generatePaths(String s, String t) throws IOException, InterruptedException {
 		Process p = Runtime.getRuntime().exec(
-				"python py_scripts\\hello_world.py " + s.replace(" ", "+") + " " + t.replace(" ", "+"));
+				"python py_scripts/hello_world.py " + s.replace(" ", "+") + " " + t.replace(" ", "+"));
 		p.waitFor();
 		
-		int pathCount = 0;
-		ArrayList<String> temp = new ArrayList();
+		ArrayList<Path> result = new ArrayList();
 		
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
 			String line;
@@ -24,26 +24,14 @@ public class test {
 				}
 				else {
 					holder = line.split("");
-					temp.add(holder[1].replace(",", ""));
-					temp.add(holder[3].replace("}", ""));
+					temp.add(holder[0]);
+					temp.add(holder[1]);
 				}
 			}
 		}		
 		
-		ArrayList<ArrayList<Double>> paths = new ArrayList<>();
-		paths.add(new ArrayList<>());
-		int zeroCount = 0;
-		for (int i = 0; i < temp.size(); i++) {
-			if (Double.valueOf(temp.get(i)) == 0) {
-				paths.add(new ArrayList<>());
-				zeroCount++;
-			}
-			else {
-				paths.get(zeroCount).add(Double.valueOf(temp.get(i)));
-			}
-		}
-		paths.remove(paths.size()-1);
-		return paths;
+		
+		return result;
 
 	}
 	
